3b9a3ba1eb39e5fe93f71bbb1db9d2b13473af42
narendra486 <pallanarendra486@gmail.com>
2020-04-06 13:38:58 +0530

    Update math_pwn.py

--- math_pwn.py
+++ math_pwn.py
@@ -1,11 +1,21 @@
 from pwn import *
+import re
 
-r = remote('host', 1337)
+r = remote('10.10.71.24', 1337)
 r.recvline_contains('give you your gift.')
 while True:
     s =r.recvline(False).decode()
     print(s)
-    # mathematical operations with eval function
+    
+    if 'Here' in s :
+        print(s)
+        break
+        
     s=s.replace('(','').replace(')','').replace(',','').replace('\'','').replace('>','')
-    r.sendline(str(eval(s)))
+    t=str(eval(s))
+    print(t)
+    r.sendline(t)
+r.interactive()   
+
+
 
